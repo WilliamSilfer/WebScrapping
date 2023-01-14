eafda2e554871c2d26bc63ac7ad17f396ce014a0
William Touya <49988034+WilliamSilfer@users.noreply.github.com>
2023-01-14 11:19:20 -0300

    Update scrapping_pato_louco.py
    Adição dos loops para capturar as inforamções desejadas

--- WebScrapping com site dinamico/scrapping_pato_louco.py
+++ WebScrapping com site dinamico/scrapping_pato_louco.py
@@ -10,15 +10,23 @@ headers = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 site = requests.get(url, headers = headers)
 soup = BeautifulSoup(site.content, 'html.parser')
 
+num_itens = 204
+ultima_pagina = math.ceil(int(num_itens) / 48)
 
 dict_produtos = {'Marca': [], 'Preço_normal': []
 , 'Preco_promocao':[], 'Desconto': []}
 
-item = soup.find_all('article', class_ = 'product 1 clearfix')
-contador = 1
+for i in range(1, ultima_pagina + 1):
+    url_pages = f'https://patoloco.com.br/produtos/placa-de-video/{i}'
+    site = requests.get(url_pages, headers = headers)
+    soup = BeautifulSoup(site.content, 'html.parser')
+    
+    item = soup.find_all('div', class_ = 'product-info')
+    for i in item:
+        marca = i.find('h3', class_ = 'tit').get_text().split()
+        preco_normal = i.find('p', class_ = 'price-old').get_text().split()
+        preco_promocao = i.find('p', class_ = 'price-new').get_text().split()
 
-for i in soup.find_all('h3', class_ = 'tit'):
-    find_all_example = i.get_text()
-    print(find_all_example)
+        print(f"Marca da placa: {marca} // Preço Normal: {preco_normal}, // Preço de promoção: {preco_promocao}")
 
-## Incompleto
+## Filtrar algumas informações utilizando slince e adicionar no dicionário
